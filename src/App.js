6c5e1f0a22cd3a34f1bddbb62583d63c3e818421
Matheus Martino <matheus.martino97@gmail.com>
2021-06-05 19:17:56 -0300

    Instalando dependencias.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router';
+import AddNote from './pages/AddNote';
+import Notes from './pages/Notes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/add-note" component={ AddNote } />
+      <Route path="/"  component={ Notes } />
+    </Switch>
   );
 }
 
